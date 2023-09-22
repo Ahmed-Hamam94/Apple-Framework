660bb9b664343dea77bffe63bec9f00fdfcc32b3
Ahmed Hamam <ahmedelsayedhamam1@gmail.com>
2023-09-22 18:18:40 +0300

    add DetailsView

--- Apple-Frameworks/FrameworkDetailView.swift
+++ Apple-Frameworks/FrameworkDetailView.swift
@@ -8,13 +8,48 @@
 import SwiftUI
 
 struct FrameworkDetailView: View {
+    
+    var framework: Framework
+    @Binding var isShowingDetailView: Bool // binding makes isShowingDetailView equal to what ever it is in the parent view which is gridView
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+       
+        VStack {
+            HStack{
+                Spacer()
+                
+                Button {
+                    isShowingDetailView = false
+                }label: {
+                    Image(systemName: "xmark")
+                        .foregroundColor(Color(.label))
+                        .imageScale(.large)
+                        .frame(width: 44,height: 44)
+                }//: Button
+            }//: HStack
+            .padding()
+            
+            Spacer()
+            
+            FrameworkTitleView(framework: framework)
+            
+            Text(framework.description)
+                .font(.body)
+                .padding()
+            
+            Spacer()
+            
+            Button{
+                
+            }label: {
+               AFBUTTON(title: "Learn More")
+            }//: Button
+        }//: VStack
     }
 }
 
 struct FrameworkDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        FrameworkDetailView()
+        FrameworkDetailView(framework: MockData.sampleFramework, isShowingDetailView: .constant(false))
     }
 }
